79f759de599ed6b8eab0c05e29057d5a08138612
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2023-10-05 22:32:13 +0300

    hook useState

--- base-course/cost-accounting/src/components/Costs/CostItem/CostItem.js
+++ base-course/cost-accounting/src/components/Costs/CostItem/CostItem.js
@@ -1,14 +1,22 @@
 import './CostItem.css'
 import CostDate from "../CostDate/CostDate";
 import Card from "../../UI/Card/Card";
+import React, { useState } from "react";
 function CostItem(props) {
+
+    const [description, setDescription] =  useState(props.description);
+    const changeDescriptionHendler = () => {
+        // description = 'New Text & call CostItem(props) function one more time.
+        setDescription('New Text');
+    }
     return (
         <Card className='cost-item'>
             <CostDate date={props.date}/>
             <div className="cost-item__description">
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className="cost-item__price">${props.amount}</div>
             </div>
+            <button onClick={changeDescriptionHendler}>Change state</button>
         </Card>
     );
 }
