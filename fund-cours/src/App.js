cc163aad05fe3455d4777ecd10794f320807baba
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2023-10-17 21:12:23 +0300

    Class components

--- fund-cours/src/App.js
+++ fund-cours/src/App.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import './App.css';
 import Counter from "./Components/Counter/Counter";
+import ClassCounter from "./Components/Counter/ClassCounter";
 
 function App() {
   return (
     <div className="App">
-        <Counter />
+        <ClassCounter />
     </div>
   );
 }
