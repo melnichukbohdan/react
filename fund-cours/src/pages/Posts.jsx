f702a58f844f12aa7e4b574ed79d32c25c7e5921
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2023-12-17 12:24:01 +0200

    Added routes

---
+++ fund-cours/src/pages/Posts.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from "react";
+import '../styles/App.css';
+import PostList from "../components/PostList/PostList";
+import PostForm from "../components/UI/PostForm/PostForm";
+import PostFilter from "../components/PostFilter/PostFilter";
+import MyModal from "../components/UI/MyModal/MyModal";
+import MyButton from "../components/UI/Button/MyButton";
+import {usePosts} from "../components/hooks/usePosts";
+import PostService from "../components/API/PostService";
+import Loader from "../components/UI/Loader/Loader";
+import {useFetching} from "../components/hooks/useFetching";
+import {getPageCount} from "../utils/pages";
+import Pagination from "../components/UI/Pagination/Pagination";
+
+function Posts() {
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit));
+    })
+
+
+    useEffect(() => {
+        fetchPosts(limit, page);
+    }, []);
+
+    const addPostHandler = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts(limit, page)
+    }
+
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>Add post</MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm onAddPost={addPostHandler} />
+            </MyModal>
+            <hr style={{margin: '15px 0'}} />
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            {postError &&
+                <h1>Error {postError}</h1>
+            }
+            {isPostsLoading
+                ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts list JS"/>
+            }
+            <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
+        </div>
+    );
+}
+
+export default Posts;
