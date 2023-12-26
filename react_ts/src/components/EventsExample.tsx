8937f877714a3a44eaf86669bf4e1d75b625a5d1
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2023-12-26 20:31:02 +0200

    useRef hook

--- react_ts/src/components/EventsExample.tsx
+++ react_ts/src/components/EventsExample.tsx
@@ -1,14 +1,15 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useRef, useState} from 'react';
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('');
     const [isDrug, setIsDrug] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        console.log(value);
+        console.log(inputRef.current?.value);
     }
 
     const drugHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -33,14 +34,16 @@ const EventsExample: FC = () => {
 
     return (
         <div>
-            <input value={value} onChange={changeHandler} type="text"/>
+            <input value={value} onChange={changeHandler} type="text" placeholder='controlled'/>
+            <input ref={inputRef} type="text" placeholder='uncontrolled'/>
             <button onClick={clickHandler}>Apply</button>
             <div onDrag={drugHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
             <div
                 onDrop={dropHandler}
                 onDragLeave={dragLeaveHandler}
                 onDragOver={dragOverHandler}
-                style={{width: 200, height: 200, background: isDrug ? 'blue' : 'red', marginTop: 15}}></div>
+                style={{width: 200, height: 200, background: isDrug ? 'blue' : 'red', marginTop: 15}}>
+            </div>
         </div>
     );
 };
